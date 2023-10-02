35cecb3bcbe9148fce1f9b72b68699c36021b373
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-02 19:00:23 +0200

    Connected supabase project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://geokxltemuyfeohbtbkl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlb2t4bHRlbXV5ZmVvaGJ0YmtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxOTUxNTEsImV4cCI6MjAxMTc3MTE1MX0.4G55oX2yxdujPTF_oaDzCU61LTzrmiK_SOj8KF0BVDA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
