c63869a2add60601d00c6247fb5318d279414682
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-11 21:26:53 +0200

    Formatted code

--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "react-hot-toast";
 import { deleteCabin as deleteCabinApi } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
 
 export function useDeleteCabin() {
   const queryClient = useQueryClient();
 
   const { isLoading: isDeleting, mutate: deleteCabin } = useMutation({
     mutationFn: deleteCabinApi,
+
     onSuccess: () => {
       toast.success("Cabin successfully deleted");
 
@@ -14,6 +15,7 @@ export function useDeleteCabin() {
         queryKey: ["cabins"],
       });
     },
+
     onError: (err) => toast.error(err.message),
   });
 
