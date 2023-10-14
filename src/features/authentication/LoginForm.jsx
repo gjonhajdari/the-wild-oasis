07acb141f883e98f41f2919e3cc94010fb549b86
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-14 23:44:47 +0200

    Removed credentials from inputs

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("gjonhajdari1@gmail.com");
-  const [password, setPassword] = useState("12345678");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoggingIn } = useLogin();
 
   function handleSubmit(e) {
